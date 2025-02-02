bee697604243866e31b2733a018021795b5a2941
OwenD1145 <119541025+OwenD1145@users.noreply.github.com>
2025-02-01 20:07:11 -0700

    Update Trade.py

--- Trade.py
+++ Trade.py
@@ -366,7 +366,7 @@ def main():
     
     timeframe = st.sidebar.selectbox(
         "Trading Timeframe",
-        ["1Min", "5Min", "15Min", "1Hour", "1Day"]
+        ["1MIN", "5MIN", "15MIN", "1HOUR", "1DAY"]
     )
     
     # Risk Management Parameters
